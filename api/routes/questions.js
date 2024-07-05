1126106dc504ad95b2263f3849974521a8d8d4d7
koreteau <77269428+koreteau@users.noreply.github.com>
2024-07-05 11:28:40 +0200

    Update questions.js
    Adding the POST method

--- api/routes/questions.js
+++ api/routes/questions.js
@@ -1,6 +1,8 @@
 // routes/questions.js
 const express = require('express');
 const Question = require('../models/Question');
+const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
 
@@ -18,6 +20,51 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+
+// @route   POST /api/questions
+// @desc    Create a new question
+// @access  Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('type.nameType', 'Question type is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title, type, status, activity } = req.body;
+
+    try {
+      const newQuestion = new Question({
+        title,
+        type,
+        status,
+        author: req.user.id,
+        activity: [
+          {
+            author: req.user.id,
+            type: 'creation',
+            date: new Date(),
+          },
+        ],
+      });
+
+      const question = await newQuestion.save();
+      res.json(question);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 
 
