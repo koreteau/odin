49e0f4e94e71d86b91ff42c907557170fb91abad
koreteau <77269428+koreteau@users.noreply.github.com>
2024-07-06 15:21:07 +0200

    Update AdminQuestions.jsx

--- app/src/components/admin/AdminQuestions.jsx
+++ app/src/components/admin/AdminQuestions.jsx
@@ -114,7 +114,7 @@ export function AdminQuestions() {
                                 return (
                                     <tr key={_id}>
                                         <td className={classes}>
-                                            <Link to={`/settings/questions/${_id}`}>
+                                            <Link to={`/register/questions/${_id}`}>
                                                 <div className="flex items-center gap-3">
                                                     <Typography variant="small" color="blue-gray" className="font-normal hover:underline">
                                                         {_id}
